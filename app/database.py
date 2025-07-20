8b2b650d58d92ab006b9ed4ec4de0d8d6d71d036
Adriano Oliveira | 2BX <140265935+adriano-2BX@users.noreply.github.com>
2025-07-19 22:20:20 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,8 @@
+# backend/app/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    database_url: str
-    class Config:
-        env_file = ".env"
-
-settings = Settings()
+from .config import settings
 
 engine = create_engine(settings.database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
